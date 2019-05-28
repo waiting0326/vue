0815f366cd3751f3775cdf7ea42d78b2dea2b6b9
waiting0326 <50769258+waiting0326@users.noreply.github.com>
2019-05-28 21:39:35 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,58 @@
+<template>
+	
+  <div id="app">
+		<input type="text" v-model="text">
+		<button @click="shanchu">按钮</button>
+		<ul>
+			<li v-for="(a,index) in list" :key="index" @click="asd(index)">
+				{{a}}
+			</li>
+		</ul>
+  </div>
+	
+</template>
+
+<script>
+	export default{
+		data(){
+			return({
+				list:[
+					"123",
+					"456",
+					"789"
+				],
+				text:''
+			})
+			
+		},
+		methods:{
+			shanchu(){
+				this.list.push(this.text);
+				this.text=""
+			},
+			asd(index){
+				this.list.splice(index,1)
+			}
+		}
+	}
+</script>
+
+<style>
+		li{
+			list-style-type: none;
+		}
+		#app{
+			margin: 0 auto;
+			width: 220px;
+			height: auto;
+			background:gray;
+		}
+		input{
+      height: 25px;;
+			background: white;
+		}
+		button{
+			background:white;
+			color:black;
+		}	
+</style>
